9a1ab778d629eccc87049f474846910f9051fcd9
Vijay Chakilam <vchakilam@adjuggler.com>
2013-07-22 22:57:16 -0400

    Initial commit

---
+++ src/main/java/com/thankrandomness/svaasya/Main.java
@@ -0,0 +1,13 @@
+package com.thankrandomness.svaasya;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
